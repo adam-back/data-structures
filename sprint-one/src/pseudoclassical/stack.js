f4d2e803faf29f18f9c4da926ab3362b9552981c
adam-back <adamback42@gmail.com>
2014-09-11 11:41:27 -0700

    Pseudoclassical stack working, but without the keywork new.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.height= 0;
+  this.stack= {};
+
 };
 
+Stack.prototype.size = function() {
+  return this.height;
+};
 
+Stack.prototype.push = function(val) {
+  this.stack[this.height] = val;
+  this.height++;
+};
+
+Stack.prototype.pop = function() {
+  var popped = this.stack[this.height - 1];
+  delete popped;
+  if(this.height > 0) {
+    this.height--;
+  }
+  return popped;
+}
