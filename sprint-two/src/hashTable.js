64d1893fed39d0d8e52668457f9cec5acfff3a3b
adam-back <adamback42@gmail.com>
2014-09-12 15:35:25 -0700

    Refactored insert to insert key-value pairs as objects within arrays. Retrieve retrieves through looping through array to match key. tl:dr Fixed insert and retrieve for one object

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,12 +5,31 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i] = v;
+  var newObj = {};
+  if(this._storage[i]!==null) {
+    this._storage[i] = [];
+    newObj[k] = v;
+    // console.log('obj', newObj,'at k', newObj[k]);
+    this._storage[i].push(newObj);
+    // console.log('store', this._storage[i]);
+  } else {
+    var prevContents = this._storage[i];
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage[i];
+
+  for(var j = 0; j < this._storage[i].length; j++) {
+    console.log('array,', this._storage[i], 'length,', this._storage[i].length);
+    for(var key in this._storage[i][j]) {
+      console.log('obj', this._storage[i][j][key]);
+      if(key===k) {
+        console.log('return',this._storage[i][j][key])
+        return this._storage[i][j][key];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
