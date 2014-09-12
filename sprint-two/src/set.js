2d496a4cd5a9ec704c67e7eb0fd3fb38bc46c1ff
adam-back <adamback42@gmail.com>
2014-09-12 11:47:47 -0700

    Set complete

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,24 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = item;
 };
+// 0(1) -- immediately accesses position in set by name
 
 setPrototype.contains = function(item){
+  return this._storage[item] === item;
 };
-
+//0(1) -- immediately accesses position in set by name
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
-
+//0(1) -- immediately accesses position in set by name
 /*
  * Complexity: What is the time complexity of the above functions?
  */
