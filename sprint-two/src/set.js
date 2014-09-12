f490d692753aae5a86eff47d0973baac3c21bbdc
Adam Back <adb33@email.arizona.edu>
2014-09-11 22:54:04 -0700

    Created set and made time complexity notations.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,27 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	if(this._storage[item] === undefined) {
+		this._storage[item] = item;
+	}
 };
+//O(1)
 
 setPrototype.contains = function(item){
+	return this._storage[item] === item;
 };
+//O(1)
 
 setPrototype.remove = function(item){
+	delete this._storage[item];
 };
+//O(1)
 
 /*
  * Complexity: What is the time complexity of the above functions?
