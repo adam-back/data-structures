3a077c71044037f00d30927f3aa4b870031a66cd
adam-back <adamback42@gmail.com>
2014-09-12 20:08:00 -0700

    Issue with children in tree, but contains is good, less tweeking after fixing addChild.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,35 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+    this.value = value;
+    this.children.push(makeTree(value));
+    console.log('kid', this.children[0], 'set', this.children);
 };
 
 treeMethods.contains = function(target){
 
+  var searchNodes = function(node) {
+    // console.log('node', node);
+    if(node.value === target) {
+      return true;
+    } else if (node.children.length>0) {
+      // console.log('children',node.children,'length', node.children.length);
+      for(var i = 0; i < node.children.length; i++) {
+        // console.log('infor', node.children[i]);
+        searchNodes(node.children[i]);
+      }
+      return false;
+    }
+  }
+  return searchNodes(this);
 };
 
 
